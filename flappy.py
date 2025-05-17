9feb14f568a42e435e0ea1f98ea6a51e290ed614
khoa <khoalun@gmail.com>
2025-05-17 12:25:48 +0700

    add reset

--- flappy.py
+++ flappy.py
@@ -34,11 +34,21 @@ pass_pipe = False
 #load images
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
+button_img = pygame.image.load('img/restart.png')
 
 
 def draw_text(text, font, text_col, x, y):
 	img = font.render(text, True, text_col)
 	screen.blit(img, (x, y))
+
+
+def reset_game():
+	pipe_group.empty()
+	flappy.rect.x = 100
+	flappy.rect.y = int(screen_height / 2)
+	score = 0
+	return score
+
 
 
 class Bird(pygame.sprite.Sprite):
@@ -110,6 +120,28 @@ class Pipe(pygame.sprite.Sprite):
 			self.kill()
 
 
+class Button():
+	def __init__(self, x, y, image):
+		self.image = image
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+
+	def draw(self):
+
+		action = False
+
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check if mouse is over the button
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1:
+				action = True
+
+		#draw button
+		screen.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
 
 bird_group = pygame.sprite.Group()
 pipe_group = pygame.sprite.Group()
@@ -118,24 +150,25 @@ flappy = Bird(100, int(screen_height / 2))
 
 bird_group.add(flappy)
 
-
+#create restart button instance
+button = Button(screen_width // 2 - 50, screen_height // 2 - 100, button_img)
 
 run = True
 while run:
 
 	clock.tick(fps)
 
-	#background
+	#draw background
 	screen.blit(bg, (0,0))
 
 	bird_group.draw(screen)
 	bird_group.update()
 	pipe_group.draw(screen)
 
-	#ground
+	#draw the ground
 	screen.blit(ground_img, (ground_scroll, 768))
 
-	#score
+	#check the score
 	if len(pipe_group) > 0:
 		if bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left\
 			and bird_group.sprites()[0].rect.right < pipe_group.sprites()[0].rect.right\
@@ -153,7 +186,7 @@ while run:
 	if pygame.sprite.groupcollide(bird_group, pipe_group, False, False) or flappy.rect.top < 0:
 		game_over = True
 
-	#check bird hit the ground
+	#check if bird has hit the ground
 	if flappy.rect.bottom >= 768:
 		game_over = True
 		flying = False
@@ -161,7 +194,7 @@ while run:
 
 	if game_over == False and flying == True:
 
-		#Pipe generation
+		#generate new pipes
 		time_now = pygame.time.get_ticks()
 		if time_now - last_pipe > pipe_frequency:
 			pipe_height = random.randint(-100, 100)
@@ -178,6 +211,13 @@ while run:
 			ground_scroll = 0
 
 		pipe_group.update()
+
+
+	#check for game over and reset
+	if game_over == True:
+		if button.draw() == True:
+			game_over = False
+			score = reset_game()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
